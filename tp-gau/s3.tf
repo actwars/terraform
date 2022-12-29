5e2c1bfea7b27502b10301ab5051479949504e59
actwars <actwars@hotmail.fr>
2022-12-29 18:30:32 +0100

    First commit

---
+++ tp-gau/s3.tf
@@ -0,0 +1,25 @@
+resource "aws_s3_bucket" "example" {
+  bucket        = var.bucket_name
+  force_destroy = true
+}
+resource "aws_s3_object" "object" {
+  bucket   = var.bucket_name
+  for_each = fileset("${var.file_site_dir}", "**")
+  key      = each.value
+  source   = "${var.file_site_dir}${each.value}"
+  etag     = filemd5("${var.file_site_dir}${each.value}")
+  acl      = "public-read"
+}
+resource "aws_s3_bucket_policy" "allow_access_from_another_account" {
+  bucket = var.bucket_name
+  policy = file("policy/policy_s3.json")
+}
+
+resource "aws_s3_bucket_website_configuration" "example" {
+  bucket = var.bucket_name
+
+  index_document {
+    suffix = "index.html"
+
+  }
+}
---
+++ tp-gau/variable.tf
@@ -0,0 +1,12 @@
+variable "region" {
+  type    = string
+  default = "us-east-1"
+}
+variable "bucket_name" {
+  type    = string
+  default = "zoulouterie-static-website"
+}
+variable "file_site_dir" {
+  type    = string
+  default = "./site/"
+}
