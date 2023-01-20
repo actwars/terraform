f1cf0c57021e3bcd1ec2b7c5e41e1fde59e7fb98
actwars <actwars@hotmail.fr>
2023-01-20 14:53:59 +0100

    Add get_dynamo.js file

---
+++ tp-gau/lambda/get_dynamo.js
@@ -0,0 +1,59 @@
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import {
+  DynamoDBDocumentClient,
+  ScanCommand,
+  PutCommand,
+  GetCommand,
+} from "@aws-sdk/lib-dynamodb";
+
+const new_visit = Date.now();
+
+const client = new DynamoDBClient({});
+
+const dynamo = DynamoDBDocumentClient.from(client);
+
+const tableName = "count_visit";
+
+export const handler = async (event, context) => {
+  let body;
+  let statusCode = 200;
+  const headers = {
+    "Content-Type": "application/json",
+  };
+
+  try {
+    switch (event.routeKey) {
+      case "GET /count":
+        body = await dynamo.send(
+          new ScanCommand({ TableName: tableName })
+        );
+//        body = body.Count;
+        break;
+      case "GET /add_count":
+//        let requestJSON = JSON.parse(event.body);
+        await dynamo.send(
+          new PutCommand({
+            TableName: tableName,
+            Item: {
+              time_stamp: new_visit,
+            },
+          })
+        );
+        body = `Put item ${new_visit}`;
+        break;
+      default:
+        throw new Error(`Unsupported route: "${event.routeKey}"`);
+    }
+  } catch (err) {
+    statusCode = 400;
+    body = err.message;
+  } finally {
+    body = JSON.stringify(body);
+  }
+
+  return {
+    statusCode,
+    body,
+    headers,
+  };
+};
