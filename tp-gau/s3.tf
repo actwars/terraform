41eb3846e8fbe741bc635cd9abb8d960f2949647
actwars <actwars@hotmail.fr>
2023-01-28 17:49:05 +0100

    Add content_type error to upload dile on s3

--- tp-gau/s3.tf
+++ tp-gau/s3.tf
@@ -8,8 +8,10 @@ resource "aws_s3_object" "object" {
   for_each = fileset("${var.file_site_dir}", "**")
   key      = each.value
   source   = "${var.file_site_dir}${each.value}"
+  content_type = lookup(tomap(local.mime_types), element(split(".", each.key), length(split(".", each.key)) - 1))
   etag     = filemd5("${var.file_site_dir}${each.value}")
   acl      = "public-read"
+
 }
 
 resource "aws_s3_bucket_policy" "allow_access_from_another_account" {
